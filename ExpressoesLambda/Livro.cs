23c3c0663f488b8f72d529c5b28346e7fee96f92
Carolina Armentano <armentanocarolina@gmail.com>
2024-01-31 15:52:31 -0300

    refactor: ToString() using nameof(property) and CultureInfo initialization.

--- ExpressoesLambda/Livro.cs
+++ ExpressoesLambda/Livro.cs
@@ -1,15 +1,20 @@
-﻿namespace ExpressoesLambda
+﻿using System.Globalization;
+
+namespace ExpressoesLambda
 {
     internal class Livro
     {
+        private readonly CultureInfo ptBr = CultureInfo.GetCultureInfo("pt-BR");
         public string Titulo { get; set; }
         public string Autor { get; set; }
         public int AnoPublicacao { get; set; }
         public decimal Preco { get; set; }
-
         public override string ToString()
         {
-            return $"Título: {Titulo}, Autor: {Autor}, AnoPublicado: {AnoPublicacao}, Preco: R$ {Preco:F2}";
+            return $"{nameof(Titulo)}: {Titulo}, " +
+                   $"{nameof(Autor)}: {Autor}, " +
+                   $"{nameof(AnoPublicacao)}: {AnoPublicacao}, " +
+                   $"{nameof(Preco)}: {Preco.ToString("C2", ptBr)}";
         }
     }
 }
